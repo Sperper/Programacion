ca569636b974b13e7404802cf6e4c93f16b1c612
sperper2907 <sperper2907@g.educaand.es>
2024-01-17 13:29:07 +0100

    Actividad peliculas 1

--- Actividades de programacion/src/com/programacion/Unidad_5/proyectoPeliculas/Practica1.java
+++ Actividades de programacion/src/com/programacion/Unidad_5/proyectoPeliculas/Practica1.java
@@ -11,7 +11,7 @@ public class Practica1 {
     public static void main(String[] args) {
         String pelisPath = "IMDB-api/Top250Movies.json";
         ArrayList<Pelicula> peliculas = JsonReaderUtil.loadJsonFile(pelisPath);
-         // Hacemos varios souts para imprimir por pantalla las opciones
+        // Hacemos varios souts para imprimir por pantalla las opciones
         System.out.println("¿Que desea hacer?");
         System.out.println("1. Imprimir las peliculas de mayor a menor nota");
         System.out.println("2. Imprimir las peliculas de menor a mayor nota");
@@ -21,35 +21,69 @@ public class Practica1 {
         System.out.println("5. Imprimir las peliculas desde el año 2000 hasta la actualidad con nota a su eleccion");
         System.out.println("6. Imprimir por pantalla una pelicula");
         System.out.println("7. Imprimir por pantalla las peliculas de un director");
-
         // Objeto Scanner para que se puede introducir un valor por teclado
         Scanner scan = new Scanner(System.in);
         int opc = scan.nextInt(); // Variable opc que permitirá introducir el número de la opcion por teclado
-
-        switch (opc) { // switch para que se realize una accion dependiendo de la opcion que se ha elegido
-
-            case 1: // si opc=1 imprime las peliculas de mayor a menor nota
-                for (int i=0; i<peliculas.size(); i++) { // Se recorre el ArrayList
-                    System.out.println(peliculas.get(i).getFullTitle()+" NOTA: "+peliculas.get(i).getImDbRating()); // Se muestra por pantalla todas las peliculas (En este caso la lista está ordenada)
+        if (opc == 1) { // si opc=1 imprime las peliculas de mayor a menor nota
+            for (int i = 0; i < peliculas.size(); i++) { // Se recorre el ArrayList
+                System.out.println(peliculas.get(i).getFullTitle() + " NOTA: " + peliculas.get(i).getImDbRating()); // Se muestra por pantalla todas las peliculas (En este caso la lista está ordenada)
+            }
+        } else if (opc == 2) { // si opc=2 imprime las peliculas de menor a mayor nota
+            for (int i = peliculas.size()-1; i >= 0; i--) {
+                System.out.println(peliculas.get(i).getFullTitle() + " NOTA: "+peliculas.get(i).getImDbRating());
+            }
+        } else if (opc == 3) {
+            for (int i=0; i<peliculas.size(); i++) {
+                if (Integer.parseInt(peliculas.get(i).getYear()) >= 2000) {
+                    System.out.println(peliculas.get(i).getFullTitle());
                 }
-            case 2: // si opc=2 imprime las peliculas de menor a mayor nota
-                for (int i=peliculas.size()-1; i>= 0; i--) { // for un poco modificado para que empiece por el ultimo valor en vez de por el primero
-                    System.out.println(peliculas.get(i).getFullTitle()+" NOTA: "+peliculas.get(i).getImDbRating()); // Muestra por pantalla als peliculas junto con su nota
+            }
+        } else if (opc == 4) { // si opc=4 imprime las peliculas ocn valoración por encima de 8.8 y del años 2000 hasta la actualidad
+            for (int i=0; i<peliculas.size(); i++) {
+                if (Integer.parseInt(peliculas.get(i).getYear()) >= 2000 && Double.parseDouble(peliculas.get(i).getImDbRating()) >= 8.8) {
+                    System.out.println(peliculas.get(i).getFullTitle());
                 }
-            case 3: // si opc=3 imprime por pantalla las peliculas ccuyo año de publicacion sea del 2000 hasta la actualidad
-                for (int i=0; i<peliculas.size(); i++) {
-                    if (Integer.parseInt(peliculas.get(i).getYear()) >= 2000) {
-                        System.out.println(peliculas.get(i).getFullTitle());
-                    }
+            }
+        } else if (opc == 5) { // si opc=5 imprime las peliculas desde el año 2000 hasta la actualidad con valoración indicada por teclado
+            System.out.println("¿Cuanta valoración tiene las peliculas que desea buscar?");
+            ArrayList<Pelicula> peliculasConVal = new ArrayList<Pelicula>();
+            double valoracionE = scan.nextDouble();
+            boolean peliculaExiste = false;
+            for (int i=0; i<peliculas.size(); i++) {
+                if (Integer.parseInt(peliculas.get(i).getYear()) >= 2000 && Double.parseDouble(peliculas.get(i).getImDbRating()) == valoracionE) {
+                    peliculasConVal.add(peliculas.get(i));
+                    peliculaExiste = true;
                 }
-
-
-
-
-
+            }
+            if (peliculaExiste) {
+                for (int i=0; i<peliculasConVal.size(); i++) {
+                    System.out.println(peliculasConVal.get(i).getTitle());
+                }
+            } else {
+                System.out.println("No existen peliculas con esa valoración");
+            }
+        } else if (opc == 6) {
+            System.out.println("Indique que pelicula desea ver");
+            String nombrePelicula = scan.next();
+            boolean peliculaExiste = false;
+            for (int i=0; i<peliculas.size(); i++) {
+                if (peliculas.get(i).getTitle().contains(nombrePelicula)) {
+                    System.out.println("TITULO: "+peliculas.get(i).getFullTitle());
+                    System.out.println("NOTA: "+peliculas.get(i).getImDbRating());
+                    System.out.println("REPARTO: "+peliculas.get(i).getCrew());
+                    System.out.println("POSICION: "+peliculas.get(i).getRank());
+                    peliculaExiste=true;
+                }
+            }
+            if (peliculaExiste) {
+                System.out.println("La pelicula no existe");
+            }
+        } else if (opc == 7) {
+            System.out.println("Escriba un director");
 
         }
 
     }
 
 }
+
