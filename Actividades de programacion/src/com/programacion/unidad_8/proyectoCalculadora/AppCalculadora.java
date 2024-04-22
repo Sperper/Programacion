ad21fddfb4d9aa6506b7bee4040e5178a48042a6
sperper2907 <sperper2907@g.educaand.es>
2024-04-22 13:33:01 +0200

    Prog

--- Actividades de programacion/src/com/programacion/unidad_8/proyectoCalculadora/AppCalculadora.java
+++ Actividades de programacion/src/com/programacion/unidad_8/proyectoCalculadora/AppCalculadora.java
@@ -1,4 +1,13 @@
 package com.programacion.unidad_8.proyectoCalculadora;
 
 public class AppCalculadora {
+
+
+    /**
+     * 600x600
+     * JPanel.NORTH -> Que ponga calculadora
+     * JPanel.CENTER
+     * JFrame -> BorderLoyout
+     * JPanel.GridLoyout
+     */
 }
